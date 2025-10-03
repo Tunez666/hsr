103cc62d7a35f3314151980e0d0155f1a010671a
Tunez <redinov14@gmail.com>
2025-10-03 20:38:39 +0800

    Главная

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+    <link rel="stylesheet" href= "main.css">
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Главная</a></li>
+            <li><a href="index.php">Сборки</a></li>
+            <li><a href="index.php">Лор</a></li>
+            <li><a href="index.php">Форум</a></li>
+        </ul>
+    </nav>
+
+    <div claass="ava">
+        <a href="lk.php">
+            <img src="ava.jpeg" />
+        </a>
+    </div>
+
+    <img class="b_pic" src="b.jpeg" alt="Личный кабинет" />
+
+    <div class="latest_ch">
+        <a href=".php">
+            <img src="ava.jpeg" />
+        </a>
+        <a href=".php">
+            <img src="ava.jpeg" />
+        </a>
+        <a href=.php">
+            <img src="ava.jpeg" />
+        </a>
+    </div>
+
+    <div class="articles">
+        <a href="">Название статьи</a>
+        <a href="">Название статьи</a>
+        <a href="">Название статьи</a>
+
+    </div>
+
+ <div class="forum">
+
+ </div>
+
+</body>
+
+</html>
